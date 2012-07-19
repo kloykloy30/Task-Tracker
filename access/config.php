38ae84465b6f1757e6ffb4e1c36360c82dfc6c93
Bruno Herfst <mail@brunoherfst.com>
2012-07-19 22:53:31 +1000

    Initial commit

---
+++ access/config.php
@@ -0,0 +1,9 @@
+<?php
+       define('DB_HOST', 'localhost');
+       define('DB_USER', 'root');
+       define('DB_PASSWORD', 'yourpassword');
+       define('DB_DATABASE', 'tasktracker');
+       //BASE_URL needs to end in a forward slash /
+       define('BASE_URL', 'http://t.oolbox.com/tasktracker/');
+       define('RESET_FILE', 'access/reset_form.php');
+?>
