b3b20c4f08747fec43610a90830abd51c60353ec
Rhys <rhys_sullivan@yahoo.com>
2012-09-09 22:02:02 +1000

    Forgot to update the services.php

--- services.php
+++ services.php
@@ -12,59 +12,55 @@
 	$cmd = $_REQUEST["cmd"];
 	if ($cmd == null) exit();
 	switch ($cmd){
+		case "mnp": // make new project;
+			$query = "INSERT INTO tt_projects (p_title) VALUES ('blank title')";
+			//	echo $query;
+			mysql_query($query);
+			$newid = mysql_insert_id();
+				
+			echo json_encode(array("id"=>$newid));
+			break;
 		case "gp": // get projects
+			$returnObj = array();
+			
+//			SELECT t1.* FROM tt_projects AS t1
+//WHERE EXISTS (SELECT * FROM tt_tags AS t2 WHERE t1.p_id = t2.tag_projectid AND t2.tag_string = 'john')
+//AND EXISTS (SELECT * FROM tt_tags AS t2 WHERE t1.p_id = t2.tag_projectid AND t2.tag_string = 'bruno')
+			
 			$result = mysql_query("SELECT NOW()");
 			$row = mysql_fetch_array($result);
-			echo "[currdate=".substr($row[0], 0, 10)."]";
+			$returnObj["currdate"] = substr($row[0], 2, 8);
 			
 			$query = "SELECT t1.* FROM tt_projects AS t1";
 			if (isset($_REQUEST['tags'])){
 				$tags = $_REQUEST["tags"];
 					
 				$tagArray = explode(",", $tags);
-				$query .= " LEFT JOIN tt_tags AS t2 ON t1.p_id = t2.tag_projectid WHERE";
+				//$query .= " LEFT JOIN tt_tags AS t2 ON t1.p_id = t2.tag_projectid WHERE";
+				$query .= " WHERE";
 				for ($i = 0; $i < sizeof($tagArray); $i++){
 					if ($i > 0) {
 						$query .= " AND";
 					}
-					$query .= " t2.tag_string='".$tagArray[$i]."'";
+					$query .= " EXISTS";
+					$query .= " (SELECT * FROM tt_tags WHERE t1.p_id = tt_tags.tag_projectid AND tt_tags.tag_string='".$tagArray[$i]."')";
 				} 
 			}
-			
+			//echo $query;
 			$result = mysql_query($query);
-			
+			$returnObj["projects"] = array();
 			if (mysql_num_rows($result) > 0 ){
-				
 				while ($row = mysql_fetch_assoc($result)){
-					
-					echo "[";
-					foreach($row as $key=>$val){
-						if ($val != null && $val != ""){
-							echo "{".substr($key, 2)."=".$val."}";
-						}
-					}
+					$row["tags"] = array();
 					$query = "SELECT tag_string FROM tt_tags WHERE tag_projectid = ".$row["p_id"];
 					$tag_result = mysql_query($query);
-					
-					echo "{tags=";
-					if (mysql_num_rows($tag_result) > 0){
-						$firstRow = true;
-						
-						while ($tag_row = mysql_fetch_array($tag_result)){
-							if ($firstRow){
-								$firstRow = false;
-							} else {
-								echo ",";
-							}
-							echo $tag_row[0];
-						}
+					while ($tag_row = mysql_fetch_array($tag_result)){
+						$row["tags"][] = $tag_row[0];
 					}
-					echo "}";
-					echo "]";
+					$returnObj["projects"][] = array_slice($row, 0);
 				}
-				
 			}
-			
+			echo json_encode($returnObj);
 			break;
 			
 		case "gt": // get tags
@@ -80,14 +76,15 @@
 			break;
 		case "sp": //save project
 			$pid = $_REQUEST["id"];
-			if (substr($pid, 0, 1) == "t"){ // if temporary id
+			if ($pid == "new"){ // if temporary id
 				$duedate = $_REQUEST["duedate"];
 				$query = "INSERT INTO tt_projects (p_title, p_duedate, p_hue, p_notes) VALUES ('".$_REQUEST["title"]."', ".(($duedate=="null")? "NULL":"'".$duedate."'").", ".$_REQUEST["hue"].", '".mysql_real_escape_string($_REQUEST["notes"])."')";
 				
 				//echo $query;
 				mysql_query($query);
 				$newid = mysql_insert_id();
-				echo "newid=".$newid."&oldid=".$pid;
+				
+				echo json_encode(array("id"=>$newid));
 				$str = "";
 				if ($_REQUEST["tags"] != ""){
 					$tags = explode(",", $_REQUEST["tags"]);
@@ -144,8 +141,20 @@
 						//echo $insert_query;
 					}
 				}
-				echo "id=".$pid;
+				echo json_encode(array("id"=>$pid));
 			}
+			break;
+		case "sb": // save bars
+			$pid 				= $_REQUEST["id"];
+			$totalmoney 		= $_REQUEST["totalmoney"];
+			$achievedmoney 		= $_REQUEST["achievedmoney"];
+			$totaltime 			= $_REQUEST["totaltime"];
+			$achievedtime 		= $_REQUEST["achievedtime"];
+			
+			$query = "UPDATE tt_projects SET p_totalmoney = $totalmoney, p_achievedmoney = $achievedmoney, p_totaltime = $totaltime, p_achievedtime = $achievedtime WHERE p_id = '$pid'";
+			
+			mysql_query($query);
+			
 			break;
 		case "dp": // delete project
 			$pid = $_REQUEST["id"];
@@ -158,21 +167,17 @@
 			$pid = $_REQUEST["id"];
 			$query = "SELECT * FROM tt_items WHERE i_projectid = ".$pid;
 			$result = mysql_query($query);
+			$returnObj = array();
 			if (mysql_num_rows($result) > 0){
 				while ($row = mysql_fetch_assoc($result)){
-					echo "[";
-					foreach($row as $key=>$val){
-						if ($val != null && $val != ""){
-							echo "{".substr($key, 2)."=".$val."}";
-						}
-					}
-					echo "]";
+					$returnObj[] = array_slice($row, 0);
 				}
 			}
+			echo json_encode($returnObj);
 			break;
 		case "si": // save item
 			$iid = $_REQUEST["id"];
-			if (substr($iid, 0, 1) == "t"){ // if temporary id
+			if ($iid == "new"){ // if temporary id
 				$query = "INSERT INTO tt_items (i_title, i_hours, i_cost, i_linkid, i_preferredduration, i_complete, i_projectid) "; 
 				$query .= 			  "VALUES ('".((isset($_REQUEST["title"]))? $_REQUEST["title"]:"")."', ";
 				$query .=						((isset($_REQUEST["hours"]))? $_REQUEST["hours"]:"NULL").", ";
@@ -182,10 +187,8 @@
 				$query .=						((isset($_REQUEST["complete"]))?$_REQUEST["complete"]:"NULL").", ";
 				$query .=						$_REQUEST["projectid"].")";
 			
-				echo $query;
 				mysql_query($query);
-				$newid = mysql_insert_id();
-				echo "newid=".$newid."&oldid=".$iid;
+				$iid = mysql_insert_id();
 				
 				if (isset($_REQUEST["linkid"])){
 					$linkid = $_REQUEST["linkid"];
@@ -201,10 +204,11 @@
 				$query .= "i_complete = ".((isset($_REQUEST["complete"]))?$_REQUEST["complete"]:"NULL").", ";
 				$query .= "i_preferredduration = ".((isset($_REQUEST["preferredduration"]))?"'".$_REQUEST["preferredduration"]."'":"NULL")." ";
 				$query .= "WHERE i_id = ".$iid;
-				echo $query;
+				//echo $query;
 				mysql_query($query);
 				//echo "id=".$iid;
 			}
+			echo json_encode(array("id"=>$iid));
 			break;
 		case "di": // delete item
 			$iid = $_REQUEST["id"];
