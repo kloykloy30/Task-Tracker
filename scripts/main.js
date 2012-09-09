5d10c6d945971e70320022eb15ea855cdc317954
Rhys <rhys_sullivan@yahoo.com>
2012-09-09 21:55:16 +1000

    Added getParameterByName function

--- scripts/main.js
+++ scripts/main.js
@@ -113,3 +113,15 @@ function searchTerms(){
 	}
 	$.post("services.php", params, onProjectLoad);
 }
+
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
